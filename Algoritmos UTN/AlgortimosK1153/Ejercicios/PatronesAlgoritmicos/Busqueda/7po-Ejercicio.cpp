7a6ec86ba6ac743ac723440f47132d9653fcd4e3
Nauel2015 <nricheri@frba.utn.edu.ar>
2025-06-26 17:40:47 -0300

    Actualizando Ejercicios.

--- Algoritmos UTN/AlgortimosK1153/Ejercicios/PatronesAlgoritmicos/Busqueda/7po-Ejercicio.cpp
+++ Algoritmos UTN/AlgortimosK1153/Ejercicios/PatronesAlgoritmicos/Busqueda/7po-Ejercicio.cpp
@@ -2,8 +2,7 @@
 /* Ejercicio N°7: Crea una estructura llamada "Factura" con los atributos "numero", "fecha" y "monto" e implementa un sistema
 donde el usuario pueda ingresar los datos de 10 facturas. Luego, el sistema solicitará al usuario que ingrese
 un número de factura a buscar. Utilizando un algoritmo de búsqueda adecuado, se determinará si la factura
-está presente en el conjunto de facturas cargadas. Si se encuentra, se mostrará su número, fecha y monto.
-Además, se brindará información adicional según el enunciado.
+está presente en el conjunto de facturas cargadas. Si se encuentra, se mostrará su número, fecha y monto. 
 
 */
 
@@ -13,10 +12,44 @@ Además, se brindará información adicional según el enunciado.
 
 using namespace std;
 
+struct tipoFecha
+{
+    int dia;
+    int mes;
+    int anio;
+};
+
+struct factura
+{
+    int numero;
+    tipoFecha fecha;
+    int monto;
+}facturas[10];
+
 // Declaracion de las funciones.
+factura cargarFacturas(factura [], int &);
+void ordenamientoBurbuja(factura [], int);
+int busquedaBinaria(factura[], int, int);
+void facturaEncontrada(factura[], int);
 
 int main()
 {
+    int contFacturas=0, opcion;
+    do{
+        cout <<"\n\tCaja \n" << endl;
+        cout <<"Opcion 1: Cargar Facturas. " << endl;
+        cout <<"Opcion 2: Buscar Factura por numero. " << endl;
+        cout <<"Opcion 3: Salir. " << endl;
+
+        cout <<"\nIngrese su opcion: "; cin >> opcion;
+
+        switch(opcion)
+        {
+            case 1: system("cls"); cargarFacturas(facturas, contFacturas); system("cls"); break;
+            case 2: system("cls"); facturaEncontrada(facturas, contFacturas); system("cls"); break;
+            case 3: cout <<"\nHasta luego. " << endl; break;
+        }
+    }while(opcion!=3);
 
     cout <<"\n- Nahuel richeri\n";
 
@@ -25,3 +58,87 @@ int main()
 }
 
 // Definicion de las funciones.
+factura cargarFacturas(factura facturas[], int &contFacturas)
+{
+    int cantFacturas;
+    cout <<"\n\tCantidad de facturas [" << contFacturas <<"]. " << endl;
+    cin.ignore();
+    cout <<"\nIngrese la cantidad de facturas a agregar: "; cin >> cantFacturas;
+
+    for(int i=0; i<cantFacturas; i++)
+    {
+        cin.ignore();
+        cout <<"\nFactura Nro " << contFacturas+1 <<". " << endl;
+        cout <<"Numero de factura: "; cin >> facturas[contFacturas].numero;
+        cout <<"\nFecha de la factura. " << endl;
+        cout <<"Dia: "; cin >> facturas[contFacturas].fecha.dia;
+        cout <<"Mes: "; cin >> facturas[contFacturas].fecha.mes;
+        cout <<"Año: "; cin >> facturas[contFacturas].fecha.anio;
+        cout <<"\nMonto de la factura: "; cin >> facturas[contFacturas].monto;
+
+        contFacturas++;
+    }
+
+    return *facturas;
+}
+
+void ordenamientoBurbuja(factura facturas[], int len)
+{
+    for(int i=0; i<len-1; i++)
+    {
+        for(int j=0; j<len-i-1; j++)
+        {
+            if(facturas[j].numero > facturas[j+1].numero)
+            {
+                factura aux = facturas[j];
+                facturas[j] = facturas[j+1];
+                facturas[j+1] = aux;
+            }
+        }
+    }
+}
+
+int busquedaBinaria(factura facturas[], int len, int buscado)
+{
+    int inicio=0, final=len-1;
+    while(inicio<=final)
+    {
+        int medio = (inicio+final)/2;
+        if(facturas[medio].numero==buscado)
+        {
+            return medio;
+        }else if(facturas[medio].numero<buscado){
+            inicio = medio+1;
+        }else{
+            final = medio-1;
+        }
+    }
+
+    return -1;
+}
+
+void facturaEncontrada(factura facturas[], int contFacturas)
+{
+    int buscado;
+    cout <<"\n\tCantidad de facturas [" << contFacturas <<"]. " << endl;
+    ordenamientoBurbuja(facturas, contFacturas);
+    cin.ignore();
+    cout <<"\nIngrese el numero de factura a buscar: "; cin >> buscado;
+    int pos = busquedaBinaria(facturas, contFacturas, buscado);
+
+    if(pos!=-1)
+    {
+        cout <<"\nFactura encontrada. " << endl;
+        cout <<"Numero de factura: " << facturas[pos].numero << endl;
+        cout <<"Fecha: " << facturas[pos].fecha.dia <<"/" << facturas[pos].fecha.mes <<"/" << facturas[pos].fecha.anio << endl;
+        cout <<"Monto: $" << facturas[pos].monto << endl;
+
+        cout <<"\nVolviendo al menu... [ENTER] " << endl;
+        system("PAUSE");
+    }else{
+        cout <<"\nFactura no encontrada. " << endl;
+
+        cout <<"\nVolviendo al menu... [ENTER] " << endl;
+        system("PAUSE");
+    }
+}
--- Algoritmos UTN/AlgortimosK1153/Ejercicios/PatronesAlgoritmicos/Busqueda/8vo-Ejercicio.cpp
+++ Algoritmos UTN/AlgortimosK1153/Ejercicios/PatronesAlgoritmicos/Busqueda/8vo-Ejercicio.cpp
@@ -15,10 +15,36 @@ buscado.
 
 using namespace std;
 
+struct alumno
+{
+    char nombre [30];
+    int edad;
+    int promedio;
+}alumnos[10];
+
 // Declaracion de las funciones.
+alumno cargarAlumnos(alumno [], int &);
+int busquedaSecuencial(alumno [], int, char []);
+void alumnoEncontrado(alumno [], int);
 
 int main()
 {
+    int contAlumnos=0, opcion;
+    do{
+        cout <<"\n\tSiu guaranie \n" << endl;
+        cout <<"Opcion 1: Cargar Alumnos. " << endl;
+        cout <<"Opcion 2: Buscar Alumno. " << endl;
+        cout <<"Opcion 3: Salir. " << endl;
+
+        cout <<"\nIngrese su opcion: "; cin >> opcion;
+
+        switch(opcion)
+        {
+            case 1: system("cls"); cargarAlumnos(alumnos, contAlumnos); system("cls"); break;
+            case 2: system("cls"); alumnoEncontrado(alumnos, contAlumnos); system("cls"); break;
+            case 3: cout <<"\nHasta luego. " << endl; break;
+        }
+    }while(opcion!=3);
 
     cout <<"\n- Nahuel richeri\n";
 
@@ -27,3 +53,114 @@ int main()
 }
 
 // Definicion de las funciones.
+alumno cargarAlumnos(alumno alumnos[], int &contAlumnos)
+{
+    int cantAlumnos;
+    cout <<"\n\tCantidad de alumnos [" << contAlumnos <<"]. " << endl;
+    cin.ignore();
+    cout <<"\nIngrese la cantidad de alumnos a cargar: "; cin >> cantAlumnos;
+
+    if(cantAlumnos+contAlumnos>=10)
+    {
+        cout <<"\nError. Solo se puede cargar hasta " << (10-contAlumnos) <<" alumnos mas. " << endl;
+
+        return *alumnos;
+        cout <<"\nVolviendo al menu..." << endl;
+        system("PAUSE");
+    }
+
+    if(contAlumnos>10)
+    {
+        cout <<"\nEl almacenamiento esta lleno. Ya no se puede cargar mas alumnos. " << endl;
+
+        return *alumnos;
+        cout <<"\nVolviendo al menu..." << endl;
+        system("PAUSE");
+    }
+
+    for(int i=0; i<cantAlumnos; i++)
+    {
+        cin.ignore();
+        cout <<"\nAlumnos Nro" << contAlumnos+1 <<". " << endl;
+        cout <<"Nombre: "; cin.getline(alumnos[contAlumnos].nombre, 30, '\n');
+        cout <<"Edad: "; cin >> alumnos[contAlumnos].edad;
+        cout <<"Promedio: "; cin >> alumnos[contAlumnos].promedio;
+
+        contAlumnos++;
+    }
+
+    return *alumnos;
+}
+
+int busquedaSecuencial(alumno alumnos[], int len, char buscado[30])
+{
+    int i=0;
+    while(i<len&&strcmp(alumnos[i].nombre, buscado)!=0)
+    {
+        i++;
+    }
+
+    if(i==len)
+    {
+        return -1;
+    }else{
+        return i;
+    }
+}
+
+void alumnoEncontrado(alumno alumnos[], int contAlumnos)
+{
+    char buscado[30];
+    int contPromedioMayor=0, contPromedioMenor=0;
+    cout <<"\n\tCantidad de alumnos [" << contAlumnos <<"]. " << endl;
+    cin.ignore();
+    cout <<"\nIngrese el nombre del alumno a buscar: "; cin.getline(buscado, 30, '\n');
+    int pos = busquedaSecuencial(alumnos, contAlumnos, buscado);
+
+    for(int i=0; i<contAlumnos; i++)
+    {
+        if(alumnos[i].promedio > alumnos[pos].promedio)
+        {
+            contPromedioMayor++;
+        }
+
+        if(alumnos[i].promedio < alumnos[pos].promedio)
+        {
+            contPromedioMenor++;
+        }
+    }
+
+    if(pos!=-1)
+    {
+        if(contPromedioMayor==0&&contPromedioMenor>=0)
+        {
+            cout <<"\nAlumno encontrado (Tiene el Mejor promedio de la comision). " << endl;
+            cout <<"Nombre: " << alumnos[pos].nombre << endl;
+            cout <<"Edad: " << alumnos[pos].edad << endl;
+            cout <<"Promedio: " << alumnos[pos].promedio << endl;
+            cout <<"\nCantidad de alumnos con menor promedio que " << alumnos[pos].nombre <<": [" << contPromedioMenor <<"]. " << endl;
+        }else if(contPromedioMayor>0&&contPromedioMenor==0){
+            cout <<"\nAlumno encontrado (Tiene el peor promedio de la comision). " << endl;
+            cout <<"Nombre: " << alumnos[pos].nombre << endl;
+            cout <<"Edad: " << alumnos[pos].edad << endl;
+            cout <<"Promedio: " << alumnos[pos].promedio << endl;
+            cout <<"\nCantidad de alumnos con mayor promedio que " << alumnos[pos].nombre <<": [" << contPromedioMayor <<"]. " << endl;
+            cout <<"\nCantidad de alumnos con menor promedio: " << contPromedioMenor <<"]. " << endl;   
+        }else{
+            cout <<"\nAlumno encontrado. " << endl;
+            cout <<"Nombre: " << alumnos[pos].nombre << endl;
+            cout <<"Edad: " << alumnos[pos].edad << endl;
+            cout <<"Promedio: " << alumnos[pos].promedio << endl;
+            cout <<"\nCantidad de alumnos con mayor promedio que " << alumnos[pos].nombre <<": [" << contPromedioMayor <<"]. " << endl;
+            cout <<"\nCantidad de alumnos con menor promedio: " << contPromedioMenor <<"]. " << endl;
+        }
+
+        cout <<"\nVolviendo al menu..." << endl;
+        system("PAUSE");
+    }else{
+        cout <<"\nNo se encontro al alumno. " << endl;
+
+        cout <<"\nVolviendo al menu..." << endl;
+        system("PAUSE");
+    }
+}
